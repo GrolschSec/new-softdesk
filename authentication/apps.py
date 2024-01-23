e3896f61ff0b3273810f736b1d985b903e99a8d6
rlouvrie <rlouvrie@student.42.fr>
2024-01-23 20:07:21 +0100

    adding a check for email model backend in settings.py for authentication app

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,6 +1,25 @@
 from django.apps import AppConfig
+from django.core.checks import register, Error
+from django.conf import settings
 
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "authentication"
+
+
+@register
+def check_backend_settings(app_configs, **kwargs):
+    errors = []
+    if (
+        "authentication.backends.EmailModelBackend"
+        not in settings.AUTHENTICATION_BACKENDS
+    ):
+        errors.append(
+            Error(
+                "EmailModelBackend not found in AUTHENTICATION_BACKENDS",
+                hint="Add 'authentication.backends.EmailModelBackend' to AUTHENTICATION_BACKENDS in settings.py",
+                id="authentication.E001",
+            )
+        )
+    return errors
