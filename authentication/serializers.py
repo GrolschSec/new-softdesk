1b8c3738f5dc67082f746123b8d0feb8bb8ca67b
rlouvrie <rlouvrie@student.42.fr>
2024-01-23 18:34:55 +0100

    adding signup view

---
+++ authentication/serializers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ["first_name", "last_name", "email", "password"]
+
+    def validate_email(self, value):
+        model = self.Meta.model
+        if model.objects.filter(email=value).exists():
+            raise serializers.ValidationError("User with this email already exists")
+        return value
+    
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create(
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            email=validated_data["email"],
+            username=validated_data["email"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
