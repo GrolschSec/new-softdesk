18e50ade9b8dd232547c10280000121b338bf12b
rlouvrie <rlouvrie@student.42.fr>
2024-01-24 13:40:27 +0100

    updating serializers

--- issues_tracking/serializers.py
+++ issues_tracking/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Project, Issue, Comment, Contributor
 
+
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
